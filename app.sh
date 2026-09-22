27ed2668ca53aa6e6e7baa7bd66e5962ac11995c
vengadeshkumar123 <vengadeshk134@gmail.com>
2026-09-22 15:23:09 +0530

    Add initial Jenkins practice application script

---
+++ app.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "================================"
+echo " Jenkins Practice Application"
+echo "================================"
+
+echo "Application Build Successful"
+echo "Application Test Successful"
